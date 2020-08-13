bbebf89289c1b16d27505d3ae87c948fe1456db7
Bjoern Mueller <bjoern.mueller@comnetgmbh.com>
2020-08-13 21:18:06 +0200

    rework main file for arduino

--- src/main.cpp
+++ src/main.cpp
@@ -2,88 +2,91 @@
 
 #include <Arduino.h>
 #include <FastLED.h>
+#include <config.h>
+#include <ArduinoJson.h>
+#include <StreamUtils.h>
+#include <sstream>
 
-#include "config.h"
+#define RXD2 17
+#define TXD2 16
 
-// Define the array of leds
 CRGB leds[NUM_LEDS];
-
 uint8_t brightness = 150;
-int button_state = 0;
 
-void simpleLEDTest();
 void runningLightTest();
+void musicReactive();
+void blur();
 
-void setup() {
+void setup() { 
 	FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
 	FastLED.setBrightness(brightness);
-
-	pinMode(BUTTON_PIN, INPUT_PULLUP);
-
-#ifdef SERIAL_PRINT
-	Serial.begin(115200);
-
-	Serial.printf("Hello World!\n");
-#endif
+    Serial.begin(115200);
+    Serial.printf("Hello World!\n");
+    // Serial2.begin(115200, SERIAL_8N1, RXD2); //, TXD2);
+    Serial2.begin(115200, SERIAL_8N1, RXD2, TXD2);
+    Serial.println("Serial Rxd is on pin: "+String(RXD2));
+    Serial.println("Serial Txd is on pin: "+String(TXD2));
+    fill_solid( leds, NUM_LEDS, CRGB::Black );
+    FastLED.show();
+      
 }
 
 void loop() {
-	runningLightTest();
-//	simpleLEDTest();
-
-	EVERY_N_MILLISECONDS(200) {
-		button_state = digitalRead(BUTTON_PIN);
-		if (button_state == LOW) {
-			brightness += 16;
-			FastLED.setBrightness(brightness);
-			Serial.printf("Brightness: %d\n", brightness);
-		}
-	}
+    // EVERY_N_MILLISECONDS(10) {
+        // blur();
+    musicReactive();
+		// runningLightTest();
+    // }
 }
 
-void simpleLEDTest() {
-#if 0
-  leds[0] = CRGB::Red;
-  leds[1] = CRGB::Green;
-  leds[2] = CRGB::Blue;
-  leds[3] = CRGB::Yellow;
-  leds[4] = CRGB::White;
-  FastLED.show();
-  delay(500);
-  leds[0] = CRGB::Black;
-  leds[1] = CRGB::Black;
-  leds[2] = CRGB::Black;
-  leds[4] = CRGB::Black;
-  FastLED.show();
-  delay(500);
-#endif
-  FastLED.showColor(CRGB::White);
-  delay(10);
 
+int i = 0;
+void musicReactive() {
+    while (Serial2.available()) {
+        uint8_t value = Serial2.read();
+        Serial.println(value);
+        if ( value == 255 || i >= NUM_LEDS) {
+            i = 0;
+            Serial2.write(84);
+            FastLED.show();
+            delayMicroseconds(20);
+            continue;
+        } else {
+            i++;
+        }
+        leds[i].setRGB(value, 0, 0);
+        // leds[i] = CHSV(random8(), 255, value);
+    }
 }
 
-uint16_t i = 0;
-int8_t speed = 1;
+
 void runningLightTest() {
-	fadeToBlackBy(leds, NUM_LEDS, 10);
-	if (speed > 0) {
-//		fadeToBlackBy(leds, i, 10);
-		leds[i] += CRGB::Red;
-	} else {
-//		Serial.printf("i: %d, other: %d\n", i, NUM_LEDS - i);
-//		fadeToBlackBy(leds + i, NUM_LEDS - i , 10);
-		leds[i] += CRGB::Blue;
-	}
-	i += speed;
-	if (i >= 148) {
-		speed = -1;
-	} else if (i == 1 && speed < 0) {
-		speed = 1;
-	}
-
+    for ( i = 0; i < NUM_LEDS; i++ ) {
+        if ( i % 10 == 0 ) {
+            leds[i] = CRGB::Red;
+        } else {
+            leds[i] = CRGB::Green;
+        }
+    }
 	FastLED.show();
 	delayMicroseconds(100);
 }
 
-void energyTest() {
-}
+void blur() {
+
+    uint8_t blurAmount = dim8_raw( beatsin8(3,64, 192) );
+    blur1d( leds, NUM_LEDS, blurAmount);
+    
+    uint8_t  i = beatsin8(  9, 0, NUM_LEDS);
+    uint8_t  j = beatsin8( 7, 0, NUM_LEDS);
+    uint8_t  k = beatsin8(  5, 0, NUM_LEDS);
+    
+    // The color of each point shifts over time, each at a different speed.
+    uint16_t ms = millis();  
+    leds[(i+j)/2] = CHSV( ms / 29, 200, 255);
+    leds[(j+k)/2] = CHSV( ms / 41, 200, 255);
+    leds[(k+i)/2] = CHSV( ms / 73, 200, 255);
+    leds[(k+i+j)/3] = CHSV( ms / 53, 200, 255);
+    FastLED.show();
+  
+} // loop()
